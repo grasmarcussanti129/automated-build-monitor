d0eb33a17a53c509116b1327e87476370140a53f
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-09-01 10:44:22 -0500

    Update response message for clarity.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.get('/', (req, res) => {
-    res.send('Automated Build Monitor is active and listening!');
+    res.send('Automated Build Monitor is active and listening on the root endpoint!');
 });
 
 io.on('connection', (socket) => {
